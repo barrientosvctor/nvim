fed33f8ed6e3f89a75e27edd7b71c148236d3fd8
barrientosvctor <vibarrientosp@gmail.com>
2023-04-07 12:48:17 -0500

    refactor: initial config improved

--- init.lua
+++ init.lua
@@ -1,34 +1,71 @@
-vim.opt.background = "dark"
-vim.g.mapleader = ","
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.termguicolors = true
-vim.opt.wrap = false
-vim.opt.laststatus = 3
-vim.opt.completeopt = "menu,menuone,noselect"
-vim.cmd("set noshowmode")
-vim.cmd("set noshowcmd")
+local options = {
+    -- Basic config
+    background = "dark",
+    number = true,
+    relativenumber = true,
+    termguicolors = true,
+    wrap = false,
+    laststatus = 3,
+    completeopt = "menu,menuone,noselect",
+    showmode = false,
+    showcmd = false,
+    colorcolumn = "80",
 
--- Spaces / indents
-vim.opt.sw = 4 -- Spaces
-vim.opt.cindent = true
-vim.opt.ai = true -- Auto indent
-vim.opt.si = true -- Smart indent
-vim.opt.breakindent = true
+    -- Spaces / indents
+    sw = 4, -- Spaces
+    cindent = true,
+    ai = true, -- Auto indent
+    si = true, -- Smart indent
+    breakindent = true,
 
--- Highlights
-vim.opt.cursorline = true
-vim.opt.winblend = 0
-vim.opt.wildoptions = "pum"
-vim.opt.pumblend = 5
+    -- Highlights
+    cursorline = true,
+    winblend = 0,
+    wildoptions = "pum",
+    pumblend = 5,
 
--- Clipboard
+    -- Search
+    hlsearch = true,
+    showmatch = true,
+    incsearch = true,
+    ignorecase = true,
+
+    -- Encoding
+    fileencoding = "utf-8",
+    encoding = "utf-8",
+
+    -- Other
+    guicursor = "n-i-v-c:block",
+    backspace = "indent,eol,start",
+    swapfile = false,
+    backup = false,
+    hidden = true,
+    scrolloff = 10,
+    undofile = true,
+}
+
+local global = {
+    mapleader = ","
+}
+
+-- vim.opt loop
+for key, value in pairs(options) do
+    vim.opt[key] = value
+end
+
+-- vim.g loop
+for key, value in pairs(global) do
+    vim.g[key] = value
+end
+
 if vim.fn.has("win32") then
     vim.opt.clipboard:prepend { "unnamed", "unnamedplus" }
+    vim.opt.undodir = "$env:LOCALAPPDATA/nvim/undodir"
 end
 
 if vim.fn.has("macunix") then
     vim.opt.clipboard:append { "unnamedplus" }
+    vim.opt.undodir = "$HOME/.config/nvim/undodir"
 end
 
 require("config.requires")
