299004c3ec2d5d4d2c76a6a9ae4acddb4dab046e
barrientosvctor <vibarrientosp@gmail.com>
2023-11-30 12:49:50 -0500

    modular config

---
+++ lua/config/options.lua
@@ -0,0 +1,65 @@
+local options = {
+    -- Basic config
+    background = "dark",
+    number = true,
+    relativenumber = true,
+    termguicolors = true,
+    wrap = false,
+    laststatus = 3,
+    completeopt = "menu,menuone,noselect",
+    showmode = false,
+    showcmd = false,
+    colorcolumn = "80",
+
+    -- Spaces / indents
+    sw = 4, -- Spaces
+    cindent = true,
+    ai = true, -- Auto indent
+    si = true, -- Smart indent
+    breakindent = true,
+
+    -- Highlights
+    cursorline = true,
+    winblend = 0,
+    wildoptions = "pum",
+    pumblend = 5,
+
+    -- Search
+    hlsearch = true,
+    showmatch = true,
+    incsearch = true,
+    ignorecase = true,
+
+    -- Encoding
+    fileencoding = "utf-8",
+    encoding = "utf-8",
+
+    -- Backup
+    swapfile = false,
+    backup = false,
+    undofile = true, -- undodir = ~/.local/state/nvim/undo/
+
+    -- Other
+    guicursor = "n-i-v-c:block",
+    backspace = "indent,eol,start",
+    hidden = true,
+    scrolloff = 10,
+}
+
+local global = {
+    mapleader = ","
+}
+
+for key, value in pairs(options) do
+    vim.opt[key] = value
+end
+
+for key, value in pairs(global) do
+    vim.g[key] = value
+end
+
+if vim.fn.has("win32") == 1 then
+    vim.opt.clipboard:prepend { "unnamed", "unnamedplus" }
+elseif vim.fn.has("unix") == 1 then
+    vim.opt.clipboard:append { "unnamedplus" }
+end
