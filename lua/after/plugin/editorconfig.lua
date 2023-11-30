822e652050cd8d52ae70338ee3701387cc142443
barrientosvctor <vibarrientosp@gmail.com>
2023-11-30 12:35:00 -0500

    delete: plugins config

--- lua/after/plugin/editorconfig.lua
+++
@@ -1,6 +0,0 @@
-local status, _ = pcall(require, "editorconfig")
-
-if (not status) then
-    print("EditorConfig is not installed.")
-    return
-end
--- lua/after/plugin/mason_lsp.lua
+++
@@ -1,7 +0,0 @@
-local status, masonLSP = pcall(require, "mason-lspconfig")
-if not status then return end
-
-masonLSP.setup({
-    ensure_installed = {},
-    automatic_installation = false,
-})
