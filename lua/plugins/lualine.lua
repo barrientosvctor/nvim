42ebb8884aa66ccdfb3a34309a97a4992934bdc4
barrientosvctor <vibarrientosp@gmail.com>
2023-12-01 17:18:55 -0500

    fix(lualine): theme changed to ayu

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = {
         options = {
-            theme = "solarized_dark",
+            theme = "ayu_dark",
             section_separators = "",
             component_separators = ""
         }
