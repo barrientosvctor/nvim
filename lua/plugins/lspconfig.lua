34c0bb11867513eda81c0b0735f91cee1140d6da
barrientosvctor <vibarrientosp@gmail.com>
2023-12-01 15:14:37 -0500

    feat(lsp): language servers secured to install

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -38,12 +38,12 @@ return {
             nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
             nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
---            nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
---            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
---            nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
---            nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
---            nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
---            nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+            --            nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+            --            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+            --            nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+            --            nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+            --            nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+            --            nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
             -- See `:help K` for why this keymap
             nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
@@ -68,9 +68,18 @@ return {
                 Lua = {
                     workspace = { checkThirdParty = false },
                     telemetry = { enable = false },
-                    -- NOTE: toggle below to ignore Lua_LS"s noisy `missing-fields` warnings
-                    -- diagnostics = { disable = { "missing-fields" } },
                 },
+            },
+            clangd = {},
+            tsserver = {},
+            pyright = {
+                python = {
+                    analysis = {
+                        autoSearchPaths = false,
+                        diagnosticMode = "openFilesOnly",
+                        useLibraryCodeForTypes = true
+                    },
+                }
             },
         }
 
