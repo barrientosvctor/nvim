275362a28be66a60c99ee6c1f04687419e90ad82
barrientosvctor <vibarrientosp@gmail.com>
2023-12-01 22:37:35 -0500

    Revert "feat(telescope): media files plugin"
    This reverts commit 8dc5fb37c18e9bd41cd50d338e6352412245d1a4.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,7 @@ return {
     tag = "0.1.5",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-file-browser.nvim",
-        "nvim-telescope/telescope-media-files.nvim"
+        "nvim-telescope/telescope-file-browser.nvim"
     },
     config = function()
         local telescope_file_browser_actions = require "telescope._extensions.file_browser.actions"
@@ -58,17 +57,11 @@ return {
                             ["?"] = "which_key",
                         },
                     }
-                },
-                media_files = {
-                    filetypes = { "png", "webp", "jpg", "jpeg" },
-                    -- find command (defaults to `fd`)
-                    find_cmd = "rg"
                 }
             },
         })
 
         require "telescope".load_extension "file_browser"
-        require "telescope".load_extension "media_files"
 
         local builtin = require "telescope.builtin"
         key.map("n", "<leader>ff", builtin.find_files, "[F]ind [F]iles via telescope")
@@ -77,7 +70,5 @@ return {
         key.map("n", "<leader>fh", builtin.help_tags, "Search into all nvim [H]elp documentation via telescope")
         key.map("n", "<leader>fv", require "telescope".extensions.file_browser.file_browser,
             "Search into all nvim [H]elp documentation via telescope")
-        key.map("n", "<leader>fm", require "telescope".extensions.media_files.media_files,
-            "Search all [M]edia via telescope")
     end
 }
