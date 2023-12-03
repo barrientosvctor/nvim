32b6fb9b9a8aa86184daaf88ed52968f28d45bd6
barrientosvctor <vibarrientosp@gmail.com>
2023-12-03 13:08:15 -0500

    perf(plugin): todo comments

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,11 +1,14 @@
-local key = require("utils.keymap")
-
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
-    config = function()
-        require "todo-comments".setup({})
-        key.map("n", "<leader>ft", ":TodoTelescope<cr>", "View all [T]ags via telescope")
-    end
+    lazy = true,
+    keys = {
+        {
+            "<leader>ft",
+            ":TodoTelescope<cr>",
+            mode = "n",
+            desc = "View all [T]ags via telescope"
+        }
+    }
 }
