bd843bcda664c524867824bee3c95ec5142b3737
barrientosvctor <vibarrientosp@gmail.com>
2023-12-03 00:35:50 -0500

    perf(plugin): gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,20 @@
 return {
     "lewis6991/gitsigns.nvim",
-    opts = {}
+    lazy = true,
+    event = "BufReadPost",
+    opts = {},
+    keys = {
+        {
+            "<leader>gb",
+            ":Gitsigns toggle_current_line_blame<cr>",
+            mode = "n",
+            desc = "Toggle git blame to the current line."
+        },
+        {
+            "<leader>gs",
+            ":Gitsigns diffthis<cr>",
+            mode = "n",
+            desc = "Show the git diff in the current file."
+        }
+    }
 }
