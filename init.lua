b4b1b5f2f0650870e0d1f3748588906369cf8394
barrientosvctor <vibarrientosp@gmail.com>
2023-12-03 13:09:07 -0500

    chore(autoformat): new filetypes added to pattern

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require("config")
 
 -- auto format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.lua" },
+    pattern = { "*.lua", "*.c", "*.cpp", "*.h", "*.hpp", "*.js", "*.ts", "*.tsx", "*.py" },
     callback = function(_)
         return vim.lsp.buf.format()
     end
