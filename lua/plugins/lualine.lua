fa27a2cfa67688332429b4bf9d200a3f675915b2
barrientosvctor <vibarrientosp@gmail.com>
2024-02-17 20:49:23 -0500

    chore(lualine): theme selection

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
     event = "VeryLazy",
     opts = {
         options = {
-            theme = "abyss",
+            theme = "auto",
             section_separators = "",
             component_separators = ""
         }
