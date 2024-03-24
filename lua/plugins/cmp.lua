64653d543e25b91403e417c6c0dd8b9f584afd29
barrientosvctor <vibarrientosp@gmail.com>
2024-03-24 16:33:33 -0500

    perf(nvim): load cmp on Insert/CmdlineEnter event

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -13,7 +13,7 @@ return {
         'rafamadriz/friendly-snippets',
     },
     lazy = true,
-    event = "InsertEnter",
+    event = { "InsertEnter", "CmdlineEnter" },
     config = function()
         -- [[ Configure nvim-cmp ]]
         -- See `:help cmp`
