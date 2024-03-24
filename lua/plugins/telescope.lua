c7079b8879ec413f2cedb15cef62e74e99ac3d95
barrientosvctor <vibarrientosp@gmail.com>
2024-03-24 17:30:08 -0500

    feat(plugin): nvim-web-devicons

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,8 @@ return {
     tag = "0.1.5",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-file-browser.nvim"
+        "nvim-telescope/telescope-file-browser.nvim",
+        "nvim-tree/nvim-web-devicons"
     },
     lazy = true,
     cmd = "Telescope",
