c455781484f856167382c75124e3852548a80b43
barrientosvctor <vibarrientosp@gmail.com>
2024-05-23 22:29:48 -0500

    feat: add fzf config, lazy attach and keymaps

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,8 +1,43 @@
 return {
-  "ibhagwan/fzf-lua",
+    "ibhagwan/fzf-lua",
     lazy = true,
-    event = "UIEnter",
-  config = function()
-    require("fzf-lua").setup({})
-  end
+    cmd = "FzfLua",
+    keys = {
+        {
+            "<leader><leader>",
+            function ()
+                require("fzf-lua").files()
+            end,
+            mode = "n",
+            desc = "Search and open files."
+        },
+        {
+            "<leader>fb",
+            function()
+                require("fzf-lua").buffers()
+            end,
+            desc = "Show the open buffers."
+        },
+        {
+            "<leader>fg",
+            function()
+                require("fzf-lua").grep()
+            end,
+            desc = "Search a pattern in entire project."
+        },
+        {
+            "<leader>fs",
+            function()
+                require("fzf-lua").git_status()
+            end,
+            desc = "Show the untaged and staged git files."
+        },
+        {
+            "<leader>fl",
+            function()
+                require("fzf-lua").builtin()
+            end
+        }
+    },
+    opts = {}
 }
