c80286b14e646260b3b7530f3f92d2c56d2b25f5
barrientosvctor <vibarrientosp@gmail.com>
2024-06-15 20:56:12 -0500

    Add unix script for colors

--- scripts/actions.sh
+++ scripts/actions.sh
@@ -6,6 +6,12 @@ if [[ $update -eq "1" ]];
 then
     git submodule init
     git submodule update
+
+    mkdir colors
+    cd colors
+    curl -O https://raw.githubusercontent.com/ayu-theme/ayu-vim/master/colors/ayu.vim
+    curl -O https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim
+    curl -O https://raw.githubusercontent.com/nordtheme/vim/main/colors/nord.vim
 elif [[ $update -eq "2" ]];
 then
     git submodule update --remote
