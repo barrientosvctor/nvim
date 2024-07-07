9f1e793f0d02c1ec556a2f6de26e33f0d044c3d5
barrientosvctor <vibarrientosp@gmail.com>
2024-07-07 00:05:08 -0500

    Add fzf settings

--- init.lua
+++ init.lua
@@ -413,7 +413,12 @@ require "telescope".setup({
     },
     pickers = {},
     extensions = {
-        fzf = {},
+        fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = true,  -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case",        -- or "ignore_case" or "respect_case" the default case_mode is "smart_case"
+        },
         file_browser = {
             theme = "dropdown",
             -- Uses Telescope's file browser instead netrw
