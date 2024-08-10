67dd6789fc889f82955552db49052b240e83873c
barrientosvctor <vibarrientosp@gmail.com>
2024-08-09 23:01:58 -0500

    chore: migrate main config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,88 +1,49 @@
-local function chooseShell()
-    if vim.fn.has "win32" == 1 then
-        if vim.fn.executable "pwsh" == 1 then
-            return "pwsh.exe"
-        elseif vim.fn.executable "powershell" == 1 then
-            return "powershell.exe"
-        else
-            return "cmd.exe"
-        end
-    else
-        if vim.fn.has "unix" == 1 or vim.fn.has "macos" == 1 then
-            return "bash"
-        end
-    end
-end
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
-local options = {
-    -- Basic config
-    background = "dark",
-    number = true,
-    relativenumber = true,
-    termguicolors = true,
-    wrap = false,
-    laststatus = 3,
-    completeopt = "menu,menuone,noinsert,noselect",
-    showmode = true,
-    showcmd = false,
-    colorcolumn = "80",
+-- Basic config
+vim.opt.background = "dark"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.wrap = true
+vim.opt.completeopt:append { "menu", "menuone", "noinsert", "noselect" }
+vim.opt.showmode = true
+vim.opt.showcmd = false
+vim.opt.colorcolumn = "80"
 
-    -- Spaces / indents
-    sw = 4,    -- Spaces
-    cindent = true,
-    ai = true, -- Auto indent
-    si = true, -- Smart indent
-    breakindent = true,
+-- Spaces / indents
+vim.opt.sw = 4
+vim.opt.ai = true -- Auto indent
+vim.opt.si = true -- Smart indent
+vim.opt.breakindent = true
 
-    -- Highlights
-    cursorline = true,
-    winblend = 0,
-    wildoptions = "pum",
-    pumblend = 5,
+-- Highlights
+vim.opt.cursorline = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
-    -- Search
-    hlsearch = true,
-    showmatch = true,
-    incsearch = true,
-    ignorecase = true,
+-- Search
+vim.opt.hlsearch = true
+vim.opt.showmatch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-    -- Encoding
-    fileencoding = "utf-8",
-    encoding = "utf-8",
+-- Encoding
+vim.opt.fileencoding = "utf-8"
+vim.opt.encoding = "utf-8"
 
-    -- Backup
-    swapfile = false,
-    backup = false,
-    undofile = true, -- undodir = ~/.local/state/nvim/undo/
+-- Backup
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true -- undodir = ~/.local/state/nvim/undo
 
-    -- Other
-    guicursor = "n-i-v-c:block",
-    backspace = "indent,eol,start",
-    hidden = true,
-    scrolloff = 5,
-
-    -- Terminal
-    shell = chooseShell()
-}
-
-local global = {
-    mapleader = ",",
-    editorconfig = true
-}
-
-for key, value in pairs(options) do
-    vim.opt[key] = value
-end
-
-for key, value in pairs(global) do
-    vim.g[key] = value
-end
-
-if vim.fn.has("win32") == 1 then
-    vim.opt.clipboard:prepend { "unnamed", "unnamedplus" }
-elseif vim.fn.has("unix") == 1 then
-    vim.opt.clipboard:append { "unnamedplus" }
-end
+-- Other
+vim.opt.backspace:append { "indent", "eol", "start" }
+vim.opt.hidden = true
+vim.opt.scrolloff = 5
+vim.opt.clipboard:append { "unnamed", "unnamedplus" }
 
 -- disable some neovim providers to improve performance
 vim.g.loaded_node_provider = 0
