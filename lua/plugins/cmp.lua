27feba6dd5745e8f38ec39a2c228fd6616fe3261
barrientosvctor <vibarrientosp@gmail.com>
2024-08-10 21:44:17 -0500

    Add lsp and completion

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,100 @@
+-- TODO: make autopairs config here
+return {
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+        -- Snippet Engine & its associated nvim-cmp source
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+
+        -- Adds LSP completion capabilities
+        'hrsh7th/cmp-nvim-lsp',
+
+        -- Adds a number of user-friendly snippets
+        'rafamadriz/friendly-snippets',
+    },
+    lazy = true,
+    event = "InsertEnter",
+    config = function()
+        local cmp = require 'cmp'
+        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+        local luasnip = require 'luasnip'
+
+        luasnip.config.set_config {
+            history = true,
+            enable_autosnippets = true,
+        }
+
+        -- NOTE: the loader is called twice so it picks up the defaults first then my custom vscode snippets.
+        -- see: https://github.com/L3MON4D3/LuaSnip/issues/364
+        require('luasnip.loaders.from_vscode').lazy_load()
+        require('luasnip.loaders.from_vscode').lazy_load({ paths = "./snippets" })
+
+        cmp.setup {
+            formatting = {
+                format = function(entry, vim_item)
+                    vim_item.kind = string.format('%s', vim_item.kind) -- This concatenates the name of the item kind
+                    -- Source
+                    vim_item.menu = ({
+                        buffer = "[Buffer]",
+                        nvim_lsp = "[LSP]",
+                        luasnip = "[LuaSnip]",
+                        nvim_lua = "[Lua]",
+                        latex_symbols = "[LaTeX]",
+                    })[entry.source.name]
+                    return vim_item
+                end
+            },
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            completion = {
+                completeopt = 'menu,menuone,noinsert'
+            },
+            mapping = cmp.mapping.preset.insert {
+                ['<C-z>'] = cmp.mapping.complete {},
+                ['<CR>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        if luasnip.expandable() then
+                            luasnip.expand()
+                        else
+                            cmp.confirm({
+                                behavior = cmp.ConfirmBehavior.Replace,
+                                select = true
+                            })
+                        end
+                    else
+                        fallback()
+                    end
+                end),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.locally_jumpable(1) then
+                        luasnip.jump(1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
+            },
+            sources = {
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+            },
+        }
+
+        -- Add parentheses after selecting function or method item
+        cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end
+}
---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,97 @@
+return {
+    -- LSP Configuration & Plugins
+    "neovim/nvim-lspconfig",
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        -- Automatically install LSPs to stdpath for neovim
+        {
+            "williamboman/mason.nvim",
+            dependencies = {
+                { "williamboman/mason-lspconfig.nvim", opts = {} },
+            },
+            build = ":MasonUpdate",
+            opts = {},
+        },
+    },
+    config = function()
+        local servers = {
+            lua_ls = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                            [vim.fn.stdpath("config") .. "/lua"] = true,
+                        },
+                    },
+                    telemetry = { enable = false },
+                },
+            },
+            clangd = {},
+            tsserver = {},
+            pyright = {
+                python = {
+                    analysis = {
+                        autoSearchPaths = false,
+                        diagnosticMode = "openFilesOnly",
+                        useLibraryCodeForTypes = true
+                    },
+                }
+            },
+            cmake = {},
+        }
+        local on_attach = function(_, bufnr)
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = '[R]e[n]ame', buffer = bufnr })
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ction', buffer = bufnr })
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover Documentation', buffer = bufnr })
+            vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, { desc = 'Signature Documentation', buffer = bufnr })
+            vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { desc = 'Go to [N]ext [D]iagnostic', buffer = bufnr })
+            vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { desc = 'Go to [P]revious [D]iagnostic', buffer = bufnr })
+
+            vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]efinition', buffer = bufnr })
+            vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences', buffer = bufnr })
+            vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations, { desc = '[G]oto [I]mplementation', buffer = bufnr })
+            vim.keymap.set('n', '<leader>D', require('telescope.builtin').lsp_type_definitions, { desc = 'Type [D]efinition', buffer = bufnr })
+            vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, { desc = '[D]ocument [S]ymbols', buffer = bufnr })
+            vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc = '[W]orkspace [S]ymbols', buffer = bufnr })
+
+            -- Lesser used LSP functionality
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = '[G]oto [D]eclaration', buffer = bufnr })
+            vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, { desc = '[W]orkspace [A]dd Folder', buffer = bufnr })
+            vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, { desc = '[W]orkspace [R]emove Folder', buffer = bufnr })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "Format", function()
+                return vim.lsp.buf.format()
+            end,
+                { desc = "Auto format the actual buffer." })
+        end
+
+        local capabilities = vim.tbl_deep_extend("force",
+            vim.lsp.protocol.make_client_capabilities(),
+            require('cmp_nvim_lsp').default_capabilities()
+        )
+
+        local mason_lspconfig = require "mason-lspconfig"
+
+        require("mason").setup()
+        mason_lspconfig.setup {
+            ensure_installed = vim.tbl_keys(servers),
+        }
+
+        -- TODO: remake lspconfig setup removing mason lspconfig
+        mason_lspconfig.setup_handlers {
+            function(server_name)
+                require("lspconfig")[server_name].setup {
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                    settings = servers[server_name],
+                    filetypes = (servers[server_name] or {}).filetypes,
+                }
+            end,
+        }
+    end
+}
