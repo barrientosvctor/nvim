9cb0c4349f3290b7afab05b1a4d7118dd7e474a6
barrientosvctor <vibarrientosp@gmail.com>
2024-08-10 20:44:15 -0500

    Add gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,20 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    lazy = true,
+    event = { "BufReadPre", "BufNewfile" },
+    opts = {},
+    keys = {
+        {
+            "<leader>gb",
+            "<Cmd>Gitsigns toggle_current_line_blame<CR>",
+            mode = "n",
+            desc = "Toggle git blame to the current line."
+        },
+        {
+            "<leader>gs",
+            "<Cmd>Gitsigns diffthis<CR>",
+            mode = "n",
+            desc = "Show the git diff in the current file."
+        }
+    }
+}
