faeaa6f3d2ac1f6fdd1cbe3283c06c17dff12e4e
barrientosvctor <vibarrientosp@gmail.com>
2024-08-31 10:54:12 -0500

    Add abyss colorscheme switcher

--- lua/plugins/abyss.lua
+++ lua/plugins/abyss.lua
@@ -1,7 +1,11 @@
+---@type boolean
+local enable_abyss = true
+
 return {
     "barrientosvctor/abyss.nvim",
     dev = true,
     lazy = false,
+    enabled = enable_abyss,
     priority = 1000,
     opts = {}
 }
