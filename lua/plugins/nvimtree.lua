3c6c82e7f4d65415ceafcb32611c97fbb4d4af34
barrientosvctor <vibarrientosp@gmail.com>
2024-09-08 14:58:46 -0500

    Fix nvimtree keymap

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -12,7 +12,9 @@ return {
     keys = {
         {
             "<Leader>fv",
-            "<Cmd>NvimTreeToggle<CR>",
+            function()
+                require"nvim-tree.api".tree.toggle({ find_file = true, update_root = true })
+            end,
             mode = "n",
             desc = "Open NvimTree"
         },
