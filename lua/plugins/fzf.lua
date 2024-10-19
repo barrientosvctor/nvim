f19d0e8a4c867c442603162d953030c1c28f89ac
barrientosvctor <vibarrientosp@gmail.com>
2024-10-19 16:09:16 -0500

    Replace fzf-lua with fzf.vim

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,46 +1,39 @@
 return {
-    "ibhagwan/fzf-lua",
+    "junegunn/fzf.vim",
+    dependencies = { "junegunn/fzf" },
     lazy = true,
     keys = {
         {
             "<Leader>ff",
-            "<Cmd>FzfLua files<CR>",
+            "<Cmd>FZF<CR>",
             mode = "n",
             desc = "[F]ind [F]iles"
         },
         {
             "<Leader>fb",
-            "<Cmd>FzfLua buffers<CR>",
+            "<Cmd>Buffers<CR>",
             mode = "n",
             desc = "Lists open [B]uffers"
         },
         {
             "<Leader>fg",
-            "<Cmd>FzfLua live_grep<CR>",
+            "<Cmd>Rg<CR>",
             mode = "n",
             desc = "Uses live [G]rep"
         },
         {
             "<Leader>fh",
-            "<Cmd>FzfLua helptags<CR>",
+            "<Cmd>Helptags<CR>",
             mode = "n",
             desc = "Search into all nvim [H]elp documentation via fzf"
         },
         {
             "<Leader>fs",
             function()
-                return require("fzf-lua").files { cwd = vim.fn.stdpath "config" }
+                vim.cmd("Files " .. vim.fn.stdpath "config")
             end,
             mode = "n",
             desc = "Search into nvim dotfiles folder"
         },
     },
-    opts = {
-        "max-perf",
-        winopts = {
-            preview = {
-                default = "default"
-            }
-        }
-    }
 }
