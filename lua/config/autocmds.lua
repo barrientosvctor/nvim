97ee644596a3c2f6fb55a8affad1b5b765750f94
barrientosvctor <vibarrientosp@gmail.com>
2024-11-09 22:14:45 -0500

    Remove terminal autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,18 +6,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Set local settings for terminal buffers
-vim.api.nvim_create_autocmd("TermOpen", {
-  group = vim.api.nvim_create_augroup("custom-term-open", {}),
-  callback = function()
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    vim.opt_local.scrolloff = 0
-
-    vim.bo.filetype = "terminal"
-  end,
-})
-
 vim.api.nvim_create_autocmd("BufReadPost", {
     group = vim.api.nvim_create_augroup("toggle-colorizer", {}),
     callback = function()
