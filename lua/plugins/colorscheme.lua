04b13cf6198159e94a1a299e82e3d2f474810217
barrientosvctor <vibarrientosp@gmail.com>
2025-03-09 14:17:48 -0500

    New colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,34 @@
 return {
     {
         "barrientosvctor/abyss.nvim",
+        enabled = true,
         dev = true,
         priority = 1000,
         opts = {},
-    }
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        enabled = false,
+        priority = 1000,
+        opts = {
+            contrast = "hard",
+            italic = {
+                strings = false
+            },
+        },
+        config = function (_, opts)
+            require("gruvbox").setup(opts)
+            vim.cmd.colorscheme("gruvbox")
+        end,
+    },
+    {
+        "Mofiqul/vscode.nvim",
+        enabled = false,
+        priority = 1000,
+        opts = {},
+        config = function (_, opts)
+            require("vscode").setup(opts)
+            vim.cmd.colorscheme("vscode")
+        end,
+    },
 }
